ec90c3b15b276e4c3a61fdef25672a8126c678cd
Alexander Wiklund <alex@plexi.se>
2022-03-21 14:05:58 +0100

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useMemo } from "react";
+import "./App.css";
 
 function App() {
+  const [dark, setDark] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  const theme = {
+    color: dark ? "white" : "black",
+    backgroundColor: dark ? "black" : "white",
+  };
+
+  const double = (number) => {
+    for (let i = 0; i < 1000000000; i++) {}
+    return number * 2;
+  };
+
+  // Kommer köras varje gång App-komponenten körs igen,
+  // oavsett vilket state vi uppdaterar.
+  // const number = double(counter);
+
+  // Kommer bara köras när counter ändras
+  const number = useMemo(() => {
+    return double(counter);
+  }, [counter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={counter}
+        onChange={(event) => setCounter(event.target.value)}
+      ></input>
+      <button onClick={() => setCounter(double(counter))}>DOUBLE NUMBER</button>
+      <button onClick={() => setDark((previous) => !previous)}>
+        CHANGE THEME
+      </button>
+      <div style={theme}>{counter}</div>
+      <div>{number}</div>
     </div>
   );
 }
